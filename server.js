7983a5c4977c99e7214ed54107baa3f72d30b3f9
eswarprasadk <eswarprasadk@turito.com>
2023-09-01 22:41:30 +0530

    Apollo package added

--- server.js
+++ server.js
@@ -1,41 +1,52 @@
 const express = require("express");
 const { buildSchema } = require("graphql");
 const { graphqlHTTP } = require("express-graphql");
+const { ApolloServer } = require("apollo-server-express");
 const { makeExecutableSchema } = require("@graphql-tools/schema");
 const { loadFilesSync } = require("@graphql-tools/load-files");
 const path = require("path");
 const http = require("http");
 
-const app = express();
+// const app = express();
 
 // const root = {
 //   products: require("./products/products.model.js"),
 //   orders: require("./orders/orders.model.js"),
 // };
 
-const typeArray = loadFilesSync("**/*", {
-  extensions: ["graphql"],
-});
-const resolverArray = loadFilesSync("**/*", {
-  extensions: ["resolver.js"],
-});
-const schema = makeExecutableSchema({
-  typeDefs: typeArray,
-  resolvers: resolverArray
-});
+// app.use(
+//   "/graphql",
+//   graphqlHTTP({
+//     schema: schema,
+//     // rootValue: root,
+//     graphiql: true,
+//   })
+// );
 
-app.use(
-  "/graphql",
-  graphqlHTTP({
-    schema: schema,
-    // rootValue: root,
-    graphiql: true,
-  })
-);
-
-function startServer() {
-  const server = http.createServer(app);
-  server.listen(5000, () => console.log("Express server started"));
+async function startApolloServer() {
+  const app = express();
+  const typeArray = loadFilesSync("**/*", {
+    extensions: ["graphql"],
+  });
+  const resolverArray = loadFilesSync("**/*", {
+    extensions: ["resolver.js"],
+  });
+  const schema = makeExecutableSchema({
+    typeDefs: typeArray,
+    resolvers: resolverArray,
+  });
+  const server = new ApolloServer({
+    schema,
+  });
+  await server.start();
+  server.applyMiddleware({ app, path: "/graphql" });
+  app.listen(3000, () => console.log("Graphql-Apollo server started"));
 }
 
-startServer();
+// function startServer() {
+//   const server = http.createServer(app);
+//   server.listen(5000, () => console.log("Express server started"));
+// }
+
+// startServer();
+startApolloServer();
